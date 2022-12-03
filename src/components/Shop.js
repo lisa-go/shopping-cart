00b209a5256c52d65584286f6dc63e9b15a2dd43
Lisa <lis-zaa@live.com>
2022-12-03 02:01:15 -0600

    added prices to each item

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import ItemImage from "./ItemImage";
 import Sidebar from "./Sidebar";
+import Item from "./item";
 
 export default function Shop() {
     const [loading, setLoading] = useState(false)
 
     const [items, setItems] = useState([{}]);
+
 
     useEffect(() => {
         setLoading(true);
@@ -14,7 +15,7 @@ export default function Shop() {
             method: 'GET',
             url: 'https://maplestory.io/api/GMS/237/item?count=50&overallCategoryFilter=equip&categoryFilter=armor&subCategoryFilter=overall&cashFilter=true'
         })
-        .then(Response => {console.log(Response)
+        .then(Response => {console.log(Response.data)
             setItems(Response.data)})
         .catch(Error => {console.log(Error)})
         .finally(() => {setLoading(false)})
@@ -28,11 +29,13 @@ export default function Shop() {
              + category + 
              '&cashFilter=true'
         })
-        .then(Response => {console.log(Response)
+        .then(Response => {console.log(Response.data)
             setItems(Response.data)})
         .catch(Error => {console.log(Error)})
         .finally(() => {setLoading(false)})
     }
+
+
 
     return (
         <div id="content">
@@ -42,10 +45,11 @@ export default function Shop() {
                 {loading ? <div>loading...</div> : 
                 
                     items.map((item) => 
-                <div className="itemContainer">
-                    <ItemImage itemid={item.id} />
-                    {item.name}
-                </div>
+                    
+                <Item 
+                    item={item} 
+                    setItems={setItems} 
+                    items={items} />
                 
 
         )}
---
+++ src/components/item.js
@@ -0,0 +1,71 @@
+
+import ItemImage from "./ItemImage";
+import React, { useState } from "react";
+
+
+export default function Item({ item, setItems }){
+    
+    //* setting prices for items *//
+    if (item.id >= 1000000 && item.id <= 1010000 || //* Hats *//
+        item.id >= 1100000 && item.id <= 1110000 || //* Capes *//
+        item.id >= 1080000 && item.id <= 1090000) { //* Gloves *//
+        if (item.id % 14 === 0) {
+            item.price = 1900;
+        } 
+        else if (item.id % 8 === 0) {
+            item.price = 1500;
+        }
+        else if (item.id % 3 === 0) {
+            item.price = 2100;
+        }
+        else {
+            item.price = 1700;
+        }
+    }
+
+    if (item.id >= 1040000 && item.id <= 1050000 || //* Tops *//
+        item.id >= 1060000 && item.id <= 1070000 || //* Bottoms *//
+        item.id >= 1070000 && item.id <= 1080000) { //* Shoes *//
+        if (item.id % 30 === 0) {
+            item.price = 2500;
+        } 
+        else if (item.id % 16 === 0) {
+            item.price = 2800;
+        }
+        else if (item.id % 4 === 0) {
+            item.price = 2900;
+        }
+        else {
+            item.price = 2300;
+        }
+    }
+
+    if (item.id >= 1050000 && item.id <= 1060000) { //* Overalls *//
+        if (item.id % 20 === 0) {
+            item.price = 3700;
+        } 
+        else if (item.id % 19 === 0) {
+            item.price = 3900;
+        }
+        else if (item.id % 3 === 0) {
+            item.price = 5000;
+        }
+        else {
+            item.price = 4500;
+        }
+}
+    
+
+    
+    return (
+        <div className="itemContainer">
+            <div className="imageContainer">
+                <ItemImage itemid={item.id} />
+            </div>
+
+            <div className="itemName">{item.name}</div>
+                    
+            <div className="itemPrice">{item.price} NX</div>
+        </div>
+    )
+}
