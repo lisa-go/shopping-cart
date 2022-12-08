d5062183a83736bf22a62a49463a9b8291d2bba5
Lisa <lis-zaa@live.com>
2022-12-08 05:02:34 -0600

    home page contents complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,60 +1,20 @@
-import { useState, useEffect } from 'react';
-import banner1 from '../images/MapleStory_BloomForest.png';
-import banner2 from '../images/MapleStory_BTS_banner.png';
-import banner3 from '../images/MapleStory_New_banner.png';
-import left from '../images/chevron_left.svg';
-import right from '../images/chevron_right.svg';
+import Banner from "./Banner";
 
 export default function Home() {
-    const image = [
-         banner1 ,
-         banner2 ,
-         banner3];
-
-    const [imageIndex, setImageIndex] = useState(0);
-     
-    const handleImage = (direction) => {
-        if (direction == 'right') {
-        setImageIndex(prev => (prev + 1) % 3)
-        }
-        else if (direction == 'left') {
-        setImageIndex(prev => (prev + 2) % 3) 
-        }
-    }
-
-    useEffect(() => {
-      const interval = setInterval(() => handleImage('right'), 5000);
-      return () => clearInterval(interval);
-    }, []);
-
+    
     return (
         <div id="home">
-
-            <div id="bannerSlider">
-                <button onClick={() => handleImage('left')} className="imgnavL">
-                    <img src={left} alt="left" />
-                </button>
-
-                <img src={image[imageIndex]} alt="banner" className="banner" />
-
-                <div className="imgNavContainer">
-                    <div className="imgNav" 
-                    style={imageIndex === 0 ?{backgroundColor: 'grey'} : null}> </div>
-
-                    <div className="imgNav" 
-                    style={imageIndex === 1 ?{backgroundColor: 'grey'} : null}> </div>
-
-                    <div className="imgNav" 
-                    style={imageIndex === 2 ?{backgroundColor: 'grey'} : null}> </div>
+            <Banner />
+            <div id="homeContent">
+                <div className="welcome">
+                    Welcome to MapleStore!
                 </div>
 
-                <button onClick={() => handleImage('right')} className="imgnavR">
-                    <img src={right} alt="right" />
-                </button>
-            </div> 
+                <div className="subtext">
+                Hundreds of New Content Available Daily
+                </div>
 
-            <div id="homeContent">
-                testing
+                <a href="/shop">Shop Now!</a>
             </div> 
         </div>
     )
